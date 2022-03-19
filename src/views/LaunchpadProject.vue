a3c2ae581b6461a54e90c12b875c524c481f9e61
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-03-19 13:38:43 -0700

    Update to LaunchpadProject view

--- src/views/LaunchpadProject.vue
+++ src/views/LaunchpadProject.vue
@@ -14,9 +14,9 @@
                         clip-rule="evenodd"
                     />
                 </svg>
-                <h2 class="mb-1 text-lg font-bold">Older projects</h2>
+                <h2 class="mb-1 text-lg font-bold">Launchpad</h2>
             </div>
-            <div class="flex items-center space-x-4">
+            <div class="flex items-center space-x-6">
                 <div>
                     <img src="../assets/website-icon.svg" alt="Website icon" />
                 </div>
@@ -33,6 +33,242 @@
                     <img src="../assets/help-icon.svg" alt="Help icon" />
                 </div>
             </div>
+        </div>
+        <div class="relative h-72 mb-12">
+            <img
+                class="h-full w-full absolute inset-0"
+                src="../assets/cryptoverse2.png"
+                alt="Cryptoverse"
+            />
+            <div class="absolute z-10 p-3">
+                <div class="px-4 py-1 bg-[#FEEDEE] text-[#F1352C] rounded-md">Coming Soon</div>
+            </div>
+            <div class="absolute z-10 right-0 pt-10 pr-6">
+                <div class="w-80 px-8 py-8 rounded-xl bg-brand-gray-1 bg-opacity-75">
+                    <div class="flex items-center justify-between mb-2">
+                        <h2 class="text-2xl font-bold">Cryptoverse</h2>
+                    </div>
+                    <p class="mb-6 text-brand-gray-2 text-xs">A blockchain Fan Token for the Aplhas</p>
+                    <div>
+                        <div class="flex items-center justify-between space-x-2 mb-2">
+                            <p class="text-brand-gray-2">Progress</p>
+                            <h2 class="font-bold">2,490 Participants</h2>
+                        </div>
+                        <div class="w-full h-2 bg-brand-gray-5 rounded-full">
+                            <div
+                                class="h-2 w-1/2 bg-gradient-to-tr from-brand-blue-7 to-brand-blue-8 rounded-full"
+                            ></div>
+                        </div>
+                        <div class="flex items-center justify-between mt-2">
+                            <p>60%</p>
+                            <p>700 ATN</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex items-center mb-16 divide-x-2 divide-white divide-opacity-10">
+            <div class="flex-1">
+                <p class="mb-1 text-brand-gray-2">Sale Price</p>
+                <h2 class="text-2xl font-bold">1 C98 = 0.006 CVR</h2>
+            </div>
+            <div class="flex-1 flex items-center justify-center">
+                <div>
+                    <p class="mb-1 text-brand-gray-2">Tokens Offered</p>
+                    <h2 class="text-2xl font-bold">4,000,000.00 CVR</h2>
+                </div>
+            </div>
+            <div class="flex-1 flex items-center justify-center">
+                <div>
+                    <p class="mb-1 text-brand-gray-2">Projects Launched</p>
+                    <h2 class="text-2xl font-bold">$0.2456</h2>
+                </div>
+            </div>
+            <div class="flex-1 flex items-center justify-end">
+                <div>
+                    <div class="flex items-center space-x-2">
+                        <p class="mb-1 text-brand-gray-2">Available Allocation per user</p>
+                        <img src="../assets/information-icon.svg" alt="Info icon" />
+                    </div>
+                    <h2 class="text-2xl font-bold">4,000 CVR</h2>
+                </div>
+            </div>
+        </div>
+        <div class="flex space-x-6">
+            <div class="w-2/3">
+                <div class="mb-12">
+                    <h2 class="mb-3 font-bold text-lg">Description</h2>
+                    <h3 class="mb-3 text-brand-gray-2">HIGHLIGHTS</h3>
+                    <ul class="mb-8 pl-6 list-disc text-sm">
+                        <li>Unreal Engine 5 provides stunning photo-realistic graphics and boundless opportunities for interoperability with other metaverses.</li>
+                        <li>Collaboration with world-renowned urban city planners responsible for designing parts of real-world major cities.</li>
+                        <li>Cinematic artists recruited from world-class VFX and game studios.</li>
+                        <li>LAND alteration feature enables flexible control of digital real estate, unlocking customization through LAND Sharing and Compilation.</li>
+                        <li>Flexible building tools invite users of all skill levels to enjoy seamless and intuitive creation experiences.</li>
+                        <li>Immersive social engagement layers include the Prana system and collaborative LAND clearing incentives.</li>
+                        <li>Curated PvP Dome unlocks social viewing experiences and win2earn opportunities for p</li>
+                    </ul>
+                    <img src="../assets/highlights-img.png" alt="Highlights img" />
+                </div>
+                <div class="mb-12">
+                    <h3 class="mb-3 text-brand-gray-2">PRODUCT</h3>
+                    <h2 class="mb-3 text-lg font-bold">The Cryptoverse Vision</h2>
+                    <div class="text-sm">
+                        <p
+                            class="mb-6"
+                        >To date, metaverses have offered the world a glimpse of what is possible, however, they’re all fairly niche, as most Metaverses are tailored for either business or retail consumption. Not both.</p>
+                        <p
+                            class="mb-6"
+                        >The Cryptoverse, however, will be designed to empower, enrich and enhance all manner of users. From brands and businesses to gamers and socialites — combining the best aspects of virtual worlds so that Cryptoversians will enjoy a plethora of explosive imagination. Social layers. Gamification. Ownership. Interoperability. Seamlessness. Avatars. Vehicles. Buildings. Everything. All in one place.</p>
+                    </div>
+                    <img src="../assets/product-img.png" alt="Product img" />
+                </div>
+                <div class="mb-12 text-sm">
+                    <h3 class="mb-3 text-brand-gray-2 text-base">NFT UTILITY</h3>
+                    <h2 class="mb-3 text-lg font-bold">Not Even Your Imagination is The Limit</h2>
+                    <div>
+                        <p
+                            class="mb-6"
+                        >By owning a piece of digital real estate, you will become part of an increasingly powerful movement that transcends how we currently view the concepts of ownership and experience.</p>
+                        <p
+                            class="mb-6"
+                        >Your digital real estate will empower you to have provable ownership, verified by distributed ledger technology (the blockchain), and access your own virtual space inside The Cryptoverse. Once here, your opportunities for creation are enormous, as users will be able to create experiences and monetize them limitlessly.</p>
+                    </div>
+                    <p>Some examples of content a user might typically look to create include:</p>
+                    <ul class="mb-8 pl-6 list-disc text-sm">
+                        <li>Fashion/digital wearable shops</li>
+                        <li>Games (both small and large)</li>
+                        <li>Marketplaces (NFTs or other assets)</li>
+                        <li>Mixed reality fast-food and restaurant experiences</li>
+                        <li>In-world VR experiences: fancy a bungee jump off a building taller than anything in the physical world?</li>
+                        <li>Watersports, snowsports, and other popular virtual sporting possibilities</li>
+                        <li>Virtual office meetings - why go to a physical space when a virtual space is so much more fun?</li>
+                    </ul>
+                    <p
+                        class="mb-6"
+                    >In the future, users will also have the possibility of subdividing their Estates or individual parcels, which affords greater opportunities for renting or outsourcing content creation. In other words, if you can’t think of what to create, why not work with somebody you know is super-creative? Even your imagination is not the limit.</p>
+                </div>
+                <div class="mb-12 text-sm">
+                    <h3 class="mb-3 text-brand-gray-2 text-base">LAND DESCRIPTION</h3>
+                    <h2 class="mb-3 text-lg font-bold">A Virtual World of Collective Imagination</h2>
+                    <div>
+                        <p
+                            class="mb-6"
+                        >The Cryptoverse Map is segmented into two layers: the first being the ‘continent layer,’ visible on the map and indicated by the different islands of the archipelago. Each of these islands is representative of one of the various Zones. In other words, each island is a different Zone.</p>
+                        <p
+                            class="mb-6"
+                        >The second layer consists of estates, which are composed of several ‘LAND parcels.’ The estates are also visible on the map and can be understood by the partitions marked around their perimeter (the lines at the edges of the estates). These partitions typically represent roads or other Foundation-owned segments.</p>
+                        <p
+                            class="mb-6"
+                        >On the second layer, there is a range of different Estate sizes available for purchase by users. The Estate sizes, and the typical or average number of individual parcels that establish the estate as a whole, are described below. A user should understand the flow: continent > estate > parcel (or land).</p>
+                        <p
+                            class="mb-6"
+                        >In public pre-sales, users will be able to purchase estates ranging from Epic to single. With an epic Estate being made up of an average of 490 parcels and a single estate being made up of 1 parcel.</p>
+                    </div>
+                </div>
+                <div class="mb-12 text-sm">
+                    <h3 class="mb-3 text-brand-gray-2 text-base">PROBLEMS</h3>
+                    <h2 class="mb-3 text-lg font-bold">User adoption is the main challenge</h2>
+                    <p
+                        class="mb-6"
+                    >Mass user adoption to decentralized metaverses has proven challenging for a number of reasons, including, but not limited to:</p>
+                    <ul class="mb-8 pl-6 list-disc text-sm">
+                        <li>Metaverses are typically tailored towards B2C or B2B rather than both</li>
+                        <li>Lack of consistently engaging opportunities and experiences</li>
+                        <li>Underwhelming incentive systems hinder adoption and user retention</li>
+                    </ul>
+                </div>
+                <div class="mb-12 text-sm">
+                    <h3 class="mb-3 text-brand-gray-2 text-base">SOLUTIONS</h3>
+                    <h2
+                        class="mb-3 text-lg font-bold"
+                    >Empowerment through modding tools, engaging social layers and support for businesses building out their metaverse presence</h2>
+                    <p
+                        class="mb-6"
+                    >The Cryptoverse aims to tackle some of these challenges through the following features:</p>
+                    <ul class="mb-8 pl-6 list-disc text-sm">
+                        <li>A custom-built modding tool enabling more technically-savvy users to create bespoke experiences; whilst also empowering those with less technical skills to terraform their environment (drag and drop assets in the world)</li>
+                        <li>Supporting brands and businesses to build out their digital real estate with high-quality content as well as meaningful content populating foundation owned areas built in tandem with a world-renowned architectural company</li>
+                        <li>Creative world-building including an infrastructure layer composed of various travel modes such as sea, air, land and subaquatic. Moreover, engaging social and gamification layers enhance users’ experiences, including Prana (Karma) system and collaborative LAND Clearing activities.</li>
+                    </ul>
+                    <img src="../assets/solutions-img.png" alt="Solutions photo" />
+                </div>
+                <div class="mb-12 text-sm">
+                    <h3 class="mb-3 text-brand-gray-2 text-base">BUSINESS MODEL</h3>
+                    <h2
+                        class="mb-3 text-lg font-bold"
+                    >Sustainable Revenue Systems to Incentivize Users</h2>
+                    <div>
+                        <p
+                            class="mb-6"
+                        >Funding raised through the sales will contribute towards sustainable development, including content creation and programming for high-quality experiences.</p>
+                        <p
+                            class="mb-6"
+                        >The purchase and sale of all LAND and NFT assets on the internal multi-chain NFT marketplace will be subject to a 0-5% fee (depending on the asset type in question) to support the ecosystem treasury and development.</p>
+                    </div>
+                    <p class="mb-6">Other monetization considerations include:</p>
+                    <ul class="mb-2 pl-6 list-disc text-sm">
+                        <li>Small fees for sharding and compilation of LAND to estates where developer input is required</li>
+                        <li>Asset curation and management for world-building consideration</li>
+                        <li>Business operation fees on profit margins where appropriate</li>
+                        <li>Foundation created and sold NFTs, which represent: vehicles, avatars, wearables, managed offices, and more.</li>
+                        <li>Infrastructure layer (travel) around the Cryptoverse</li>
+                        <li>Worldbuilding solutions for partner brands and businesses</li>
+                        <li>B2B ad-revenue streams in The Cryptoverse for high-traffic areas</li>
+                    </ul>
+                    <p
+                        class="mb-6"
+                    >Much of the above will be implemented with governance also maintaining an active voice in directing reasonable monetization models for The Cryptoverse.</p>
+                </div>
+                <div class="mb-12 text-sm">
+                    <h3 class="mb-3 text-brand-gray-2 text-base">MARKET</h3>
+                    <h2
+                        class="mb-3 text-lg font-bold"
+                    >Metaverse industry projected to be worth $800 billion in two years</h2>
+                    <div>
+                        <p
+                            class="mb-6"
+                        >Existing metaverse users are familiar with Sandbox, Decentraland, and NetVrk. Rather than competitors, Cryptoverse sees these pioneers as critical pillars supporting the infrastructure of metaverse and blockchain adoption. After all, a Multiverse or a collective interconnectedness of metaverses is inevitable.</p>
+                        <p
+                            class="mb-6"
+                        >The metaverse industry is projected to be worth $800 billion by 2024, according to Bloomberg, and why? Combining immersive 3D virtual world experiences with ownership, monetization opportunities, and endless adventure brings to life a new era - an era of everything for anyone. Welcome to the Cryptoverse.</p>
+                    </div>
+                </div>
+                <div class="mb-12 text-sm">
+                    <h3 class="mb-3 text-brand-gray-2 text-base">TEAM</h3>
+                    <h2 class="mb-3 text-lg font-bold">Tip-Top Team Tiptoes into The Cryptoverse</h2>
+                    <div>
+                        <p
+                            class="mb-6"
+                        >ChainGuardians brings to the Cryptoverse a core team with years of experience specifically dedicated to building a thriving platform combining the implementation of technological advances based in interoperable NFTs, blockchain-based gaming and combined experience building in early metaverses, including project integration and board-level district direction.</p>
+                        <img src="../assets/team-img.png" alt="Team img" />
+                    </div>
+                </div>
+                <div class="mb-12 text-sm">
+                    <h3 class="mb-3 text-brand-gray-2 text-base">TOKEN UTILITY</h3>
+                    <h2
+                        class="mb-3 text-lg font-bold"
+                    >$CGG is the centerpiece of the ChainGuardians ecosystem</h2>
+                    <div>
+                        <p
+                            class="mb-6"
+                        >The ChainGuardians Governance token ($CGG) underpins the impressively expansive ChainGuardians ecosystem including, ChainBoost, ChainGuardians, and now, Cryptoverse. $CGG is carefully and meticulously crafted to maximize freedom of choice, utility, and engagement within the interconnected ecosystems.</p>
+                    </div>
+                    <p class="mb-6">Token Utilities include:</p>
+                    <ul class="mb-2 pl-6 list-disc text-sm">
+                        <li>Partake in governance voting (soon)</li>
+                        <li>Elevate Play-to-Earn opportunities</li>
+                        <li>Improve immersive gaming experiences</li>
+                        <li>Participate in traditional and gamified DeFi protocols</li>
+                        <li>Power and govern Cryptoverse.vip</li>
+                        <li>Increase access to initial offerings through ChainBoost.io</li>
+                        <li>Cover NFT minting fees</li>
+                        <li>Purchase physical merchandise</li>
+                        <li>Enjoy cross-chain flexibility (Polygon, Ethereum, BSC & more on the way!)</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="w-1/3"></div>
         </div>
     </div>
 </template>
