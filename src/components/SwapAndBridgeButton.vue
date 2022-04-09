6a14a3a5a769bc33209c3c52eb8f301463667bfa
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-04-09 11:20:08 -0700

    Adding of functionality to swapandbrdgebutton

--- src/components/SwapAndBridgeButton.vue
+++ src/components/SwapAndBridgeButton.vue
@@ -4,11 +4,17 @@
             class="flex items-center justify-between space-x-2 w-80 h-14 p-2 bg-brand-gray-1 text-sm rounded-full"
         >
             <div
-                class="flex-1 flex justify-center h-10 bg-brand-gray-5 text-white font-bold rounded-full"
+                @click="switchTab('swap')"
+                :class="selectedTab === 'swap' ? 'h-10 bg-brand-gray-5 text-white font-bold rounded-full' : 'text-brand-gray-3'"
+                class="flex-1 flex justify-center"
             >
                 <button>Swap</button>
             </div>
-            <div class="flex-1 flex justify-center text-brand-gray-3">
+            <div
+                @click="switchTab('bridge')"
+                :class="selectedTab === 'bridge' ? 'h-10 bg-brand-gray-5 text-white font-bold rounded-full' : 'text-brand-gray-3'"
+                class="flex-1 flex justify-center"
+            >
                 <button>Bridge</button>
             </div>
         </div>
@@ -16,12 +22,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, defineEmits, ref } from 'vue'
 
 export default defineComponent({
     name: "SwapAndBridgeButton",
-    setup() {
 
+    setup(props, { emit }) {
+        const selectedTab = ref('')
+        const switchTab = (tab: string) => {
+            emit("changeTab", tab)
+            selectedTab.value = tab
+        }
+
+        return { selectedTab, switchTab }
     },
 })
 </script>
--- src/views/Exchange.vue
+++ src/views/Exchange.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <SwapAndBridgeButton />
-        <div v-if="swap">
+        <SwapAndBridgeButton @changeTab="selectTab" />
+        <div v-show="selectedTab === 'swap'">
             <Swap />
         </div>
-        <div v-if="bridge">
+        <div v-show="selectedTab === 'bridge'">
             <Bridge />
         </div>
         <SwapInfo />
@@ -22,9 +22,13 @@ import Bridge from '../components/Bridge.vue'
 export default defineComponent({
     components: { SwapAndBridgeButton, Swap, SwapInfo, Bridge },
     setup() {
-        const bridge = ref(true)
-        const swap = ref(false)
-        return { swap, bridge }
+        const selectedTab = ref('swap')
+        const selectTab = (tab: string) => {
+            selectedTab.value = tab
+            console.log("clicked", selectedTab.value)
+
+        }
+        return { selectedTab, selectTab }
     },
 })
 </script>
